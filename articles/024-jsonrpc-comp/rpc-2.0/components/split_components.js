422d22dbaaa2c6f9799342d0da475aaec83c44a0
Mel Padden <paddentech@gmail.com>
2024-07-15 01:45:41 +0200

    moved script, corrected typo

--- articles/024-jsonrpc-comp/rpc-2.0/components/split_components.js
+++
@@ -1,8 +0,0 @@
-var componentsFile = require("./components.json");
-const fs = require('node:fs');
-//console.debug(componentsFile.components.schemas);
-for (elem in componentsFile.components.schemas) {
-    var content = JSON.stringify(componentsFile.components.schemas[elem], null, 4);
-    //console.debug(content);
-    fs.writeFileSync(elem + ".json", content, 'utf8');
-}
