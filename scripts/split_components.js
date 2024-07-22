eac2796936deef8d37cde06332670f5aa3515983
Mel Padden <paddentech@gmail.com>
2024-07-23 01:34:44 +0200

    Updated rpc schema .2

--- scripts/split_components.js
+++
@@ -1,8 +0,0 @@
-var componentsFile = require("../articles/024-jsonrpc-comp/rpc-2.0/components/components.json");
-const fs = require('node:fs');
-//console.debug(componentsFile.components.schemas);
-for (elem in componentsFile.components.schemas) {
-    var content = JSON.stringify(componentsFile.components.schemas[elem], null, 4);
-    //console.debug(content);
-    //fs.writeFileSync(elem + ".json", content, 'utf8');
-}
